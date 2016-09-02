c0caf631ab81819d778adc8c8c1765d7cd9a35a9
Ross Packard <ross.packard@gmail.com>
2016-09-02 07:32:19 -0400

    Add rspec and capybara and create one simple test

---
+++ spec/features/user_creates_a_character.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+feature 'User creates a character' do
+  scenario 'they see the character on the page' do
+    hill_dwarf = Race.create!(
+      name: "Hill Dwarf",
+      size: 2,
+      speed: 30,
+      vision: 1,
+      strength_bonus: 0,
+      dexterity_bonus: 0,
+      constitution_bonus: 2,
+      wisdom_bonus: 1,
+      intelligence_bonus: 0,
+      charisma_bonus: 0)
+
+    barbarian = HeroClass.create!(
+      name: "Barbarian",
+      hit_dice: 12,
+      strength_saving_throw_proficiency: true,
+      dexterity_saving_throw_proficiency: false,
+      constitution_saving_throw_proficiency: true,
+      wisdom_saving_throw_proficiency: false,
+      intelligence_saving_throw_proficiency: false,
+      charisma_saving_throw_proficiency: false)
+
+    visit new_character_path
+
+    fill_in 'Name', with: 'JD'
+    fill_in 'Height in Inches', with: '72'
+    fill_in 'Weight in Pounds', with: '150'
+    fill_in 'Level', with: '1'
+    fill_in 'Current XP', with: '0'
+    fill_in 'Strength', with: '8'
+    fill_in 'Dexterity', with: '8'
+    fill_in 'Constitution', with: '8'
+    fill_in 'Wisdom', with: '8'
+    fill_in 'Intelligence', with: '8'
+    fill_in 'Wealth', with: '20'
+    fill_in 'Speed', with: '30'
+    fill_in 'Current HP', with: '15'
+    fill_in 'Max HP', with: '15'
+    fill_in 'Temporary HP', with: '0'
+    fill_in 'Proficiency Bonus', with: '2'
+    choose 'Normal Vision'
+    select 'Hill Dwarf', from: 'Race'
+    select 'Barbarian', from: 'Class'
+
+    click_button 'Save'
+
+    expect(page).to have_css '.notice', 'Character was saved successfully.'
+  end
+end
